f250d864c72267f83894c5cba14a525b22cfa481
Scott McWhirter <scott.mcwhirter@joyent.com>
2010-03-07 19:58:30 -0800

    Revert "If the config file is a symlink, then use the realpath()"
    This reverts commit 3aa1ea1a4f613162a32ed3d8defa070a23877e5e.

--- app.js
+++ app.js
@@ -111,11 +111,8 @@ var setup_connection = function(prev, curr) {
 };
 
 // We want to be able to change configuration on the fly, so we may need to reconnect, etc.
-posix.realpath(gitosisConfig, function(e, resolved_path) {
-    gitosisConfig = resolved_path;
-    posix.watchFile(gitosisConfig, { persistent: true, interval: 10000 }, setup_connection);
-    setup_connection();
-});
+posix.watchFile(gitosisConfig, { persistent: true, interval: 10000 }, setup_connection);
+setup_connection();
 
 var update_or_create = function(repository,projectsDir,gitUser,gitServer) {
   projectPath = path.join(projectsDir, repository);
